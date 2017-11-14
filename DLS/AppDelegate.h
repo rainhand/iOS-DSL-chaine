b2b8f046f6c9188059728f71eccc7b787244d5bf
郭超 <guochao_ios@126.com>
2017-11-14 15:48:50 +0800

    custom your message

---
+++ DLS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DLS
+//
+//  Created by 郭超 on 2017/4/20.
+//  Copyright © 2017年 郭超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DLS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DLS
+//
+//  Created by 郭超 on 2017/4/20.
+//  Copyright © 2017年 郭超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
